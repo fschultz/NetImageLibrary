356915f41641432a98bc98b7aa65d1f6cee1b96c
Fredrik Schultz <fschultz@users.noreply.github.com>
2010-01-20 19:33:05 +0000

    New test code for filters

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
-
-using System.Text;
-using Kaliko.ImageLibrary;
 using System.Drawing;
+using Kaliko.ImageLibrary;
+using Kaliko.ImageLibrary.Filters;
 
 namespace TestApp {
     class Program {
@@ -16,6 +14,17 @@ namespace TestApp {
             image.GetThumbnailImage(100, 100, ThumbnailMethod.Crop).SaveJPG("thumbnail-crop.jpg", 90);
 
             image.GetThumbnailImage(100, 100, ThumbnailMethod.Pad).SaveJPG("thumbnail-pad.jpg", 90);
+
+            KalikoImage sharpimg = image.GetThumbnailImage(100, 100, ThumbnailMethod.Crop);
+            sharpimg.ApplyFilter(new UnsharpMaskFilter(1.2, 0.3));
+            sharpimg.SaveJPG("thumbnail-unsharpened.jpg", 90);
+
+            sharpimg.ApplyFilter(new DesaturationFilter());
+            sharpimg.SaveJPG("thumbnail-gray.jpg", 90);
+
+            sharpimg.ApplyFilter(new ContrastFilter(30));
+            sharpimg.SaveJPG("thumbnail-contrast.jpg", 90);
+
         }
     }
 }
